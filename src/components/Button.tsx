86a29ae76d76a586f703792e7ceed8066ca3fd0f
Guilherme Takahashi <gui.yukio175@gmail.com>
2023-01-08 12:54:58 -0300

    feat: use button variant

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -10,20 +10,27 @@ type Props = IButtonProps & {
 
 export function Button({ 
   title,
+  variant,
   ...rest
 }: Props) {
   return (
     <NativeBaseButton
       w="full"
       h={14}
-      bg="green.700"
+      bg={variant === "outline" ? "transparent" : "green.700"}
+      borderWidth={variant === "outline" ? 1 : 0}
+      borderColor="green.500"
       rounded="sm"
       _pressed={{
-        bg: 'green.500',
+        bg: variant === "outline" ? "gray.500" : 'green.500',
       }}
       {...rest}
     >
-      <Text color="white" fontFamily="heading" fontSize="sm">
+      <Text 
+        color={variant === "outline" ? "green.500" : "white"} 
+        fontFamily="heading" 
+        fontSize="sm"
+      >
         {title}
       </Text>
     </NativeBaseButton>
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -42,6 +42,8 @@ export function SignIn() {
 
         <Button title="Acessar" />
       </Center>
+
+      <Button title="Criar conta" variant="outline" />
     </VStack>
   );
 }
