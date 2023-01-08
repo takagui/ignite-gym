ea4432612598baae88741a18cfea8e8fa59744d8
Guilherme Takahashi <gui.yukio175@gmail.com>
2023-01-08 16:46:08 -0300

    feat: create navigation context

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {
   Roboto_700Bold,
 } from '@expo-google-fonts/roboto';
 
-import { SignUp } from '@screens/SignUp';
+import { Routes } from '@routes/index'
 
 import { Loading } from '@components/Loading';
 
@@ -25,7 +25,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <SignUp /> : <Loading />}
+      { fontsLoaded ? <Routes /> : <Loading />}
     </NativeBaseProvider>
   );
 }
