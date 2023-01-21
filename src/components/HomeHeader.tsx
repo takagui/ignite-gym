26891ecd148b833357d39770f112e593d895ae2d
Guilherme Takahashi <gui.yukio175@gmail.com>
2023-01-20 23:31:44 -0300

    feat: create component user photo

--- src/components/HomeHeader.tsx
+++ src/components/HomeHeader.tsx
@@ -1,8 +1,18 @@
 import { Heading, HStack, Text, VStack } from 'native-base';
 
+import { UserPhoto } from './UserPhoto';
+
 export function HomeHeader() {
   return (
     <HStack bg="gray.660" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto 
+        source={{
+          uri: 'https://github.com/takagui.png'
+        }}
+        alt="Imagem do usuário"
+        size={16}
+        mr={4}
+      />
 
       <VStack>
         <Text color="gray.100" fontSize="md">
