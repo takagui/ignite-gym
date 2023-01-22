9d29d64990d1dba684815ab58e89d8630f2a59d2
Guilherme Takahashi <gui.yukio175@gmail.com>
2023-01-21 23:26:47 -0300

    feat: create component group

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,14 @@
-import { HomeHeader } from '@components/HomeHeader';
 import { VStack } from 'native-base';
+
+import { HomeHeader } from '@components/HomeHeader';
+import { Group } from '@components/Group';
 
 export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
+
+      <Group name="costas" />
     </VStack>
   )
 }
