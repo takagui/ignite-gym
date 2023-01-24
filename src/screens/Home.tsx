c887e1f971ae9ea370c2dbf6240d758241669eb4
Guilherme Takahashi <gui.yukio175@gmail.com>
2023-01-23 21:53:01 -0300

    feat: list groups

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,32 +1,36 @@
 import { useState } from 'react';
-import { HStack, VStack } from 'native-base';
+import { VStack, FlatList } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
 
-type TGroupType = 'costa' | 'ombro' | 'tríceps'; 
+type TGroupType = 'costa' | 'ombro' | 'tríceps' | 'bíceps'; 
 
 export function Home() {
+  const [groups, setGroups] = useState<TGroupType[]>(['costa', 'ombro', 'tríceps', 'bíceps']);
   const [groupSelected, setGroupSelected] = useState<TGroupType>('costa');
-  
-  const groups: TGroupType[] = ['costa', 'ombro', 'tríceps'];
 
   return (
     <VStack flex={1}>
       <HomeHeader />
 
-      <HStack>
-        {
-          groups.map(group => (
-            <Group 
-              key={group}
-              name={group}
-              isActive={groupSelected === group} 
-              onPress={() => setGroupSelected(group)}
-            />
-          ))
-        }
-      </HStack>
+      <FlatList
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <Group 
+            key={item}
+            name={item}
+            isActive={groupSelected === item} 
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
     </VStack>
   )
 }
