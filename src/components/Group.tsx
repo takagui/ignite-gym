2439ec52635f01dc0920d5f6bf3e96aff8d5c1df
Guilherme Takahashi <gui.yukio175@gmail.com>
2023-01-23 21:35:07 -0300

    feat: create group button

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -1,20 +1,37 @@
-import { Text } from 'native-base';
+import { Text, Pressable, IPressableProps } from 'native-base';
 
-type Props = {
+type Props = IPressableProps & {
   name: string;
 }
 
 export function Group({
   name,
+  ...rest
 }: Props) {
   return (
-    <Text
-      color="gray.200"
-      textTransform="uppercase"
-      fontSize="xs"
-      fontWeight="bold"
+    <Pressable
+      mr={3}
+      w={24}
+      h={10}
+      bg="gray.600"
+      rounded="md"
+      justifyContent="center"
+      alignItems="center"
+      overflow="hidden"
+      _pressed={{
+        borderColor: "green.500",
+        borderWidth: 1,
+      }}
+      {...rest}
     >
-      {name}
-    </Text>
+      <Text
+        color="gray.200"
+        textTransform="uppercase"
+        fontSize="xs"
+        fontWeight="bold"
+      >
+        {name}
+      </Text>
+    </Pressable>
   )
 }
